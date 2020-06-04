4b22ed8d9347fd948914015c7f67310adce508f0
jtuusnas <joonas.t.uusnakki@student.jyu.fi>
2020-06-04 17:18:12 +0300

    3c done

--- index.js
+++ index.js
@@ -1,117 +1,95 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
 const Person = require('./models/person')
 
 morgan.token('personNumber', function(req, res){
   return JSON.stringify(req.body)
 })
 
-app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :personNumber'))
 
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramos",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mick Jackson",
-    number: "033-222-123",
-    id: 6
-  },
-  {
-    name: "Joonas Uusnäkki",
-    number: "0442966342",
-    id: 7
-  },
-  {
-    name: "Aki Nyberg",
-    number: "040-5567893",
-    id: 8
-  }
-]
-
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(persons => {
-    res.json(persons.map(p => p.toJSON()))
+    res.json(persons.map(person => person.toJSON()))
   })
 })
 
 app.get('/info', (req, res) => {
-  console.log('moro')
-  let content = persons.length
+  console.log('infoa tulossa')
   let newDate = new Date()
-
-  res.send('<p>Phonebook has info for ' + content + ' people</p>' + newDate)
+  Person.find({}).then(persons => {
+    let people = persons.map(p => p.toJSON()).length
+    res.send('<p>Phonebook has info for ' + people + ' people</p>' + newDate)
+  })
   console.log('näkemiin')
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then(p => {
-    res.json(p)
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+  .then(p => {
+    if(p) {
+      res.json(p)
+    } else {
+      res.status(404).end()
+    } 
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(p => p.id !== id)
-
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+  .then(() => {
+    res.status(204).end()
+  })
+  .catch(error => next(error))
 })
 
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
 
-  if(body.name === undefined || body.number === undefined) {
-    return res.status(400).json({
-      error: 'person missing'
-    })
-  }
-  if(persons.map(p => p.name.toLowerCase()).indexOf(body.name.toLowerCase()) !== -1){
-    console.log('nimi jo käytössä')
-    return res.status(400).json({
-      error: 'name must be unique'
-    })
-  }
   const p = new Person({
     name: body.name,
     number: body.number,
-    id: generateId()
+    //id: generateId()
   })
-
-  persons = persons.concat(p)
-  console.log("APua")
+  p.save()
+  .then(savedPerson => {
+    res.json(savedPerson)
+  })
+  .catch(error => next(error))
   console.log(body)
-
-  res.json(p)
   console.log("Apuaaaa")
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'vääränmuotoinen id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-mongoose.set('useFindAndModify', false)
-const url = process.env.MONGODB_URI
 
+const url = process.env.MONGODB_URI
+console.log(require('dotenv').config())
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => {
+.then(result => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -13,16 +13,16 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   })
 
   const personSchema = new mongoose.Schema({
-    name: {type: String},
-    number: {type: String},
+    name: String,
+    number: String
   })
-
+  
   personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
 
 module.exports = mongoose.model('Person', personSchema)
